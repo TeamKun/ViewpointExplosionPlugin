94b7deaf07a2ff702c728704a247abddd3ee58b5
paruteto <paruteto66ym@gmail.com>
2021-06-06 02:04:53 +0900

    テストコミット
    プロジェクト作成

---
+++ src/main/java/paruteto_plugin_01/viewpointexplosionplugin/ViewpointExplosionPlugin.java
@@ -0,0 +1,17 @@
+package paruteto_plugin_01.viewpointexplosionplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ViewpointExplosionPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
